5c0377156ec40258d54c926f46471cae698ad684
shubham12-prof <shubhsemwal12@gmail.com>
2025-05-06 19:39:29 +0530

    images loaded

--- src/Components/Gallary/Images.js
+++ src/Components/Gallary/Images.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import "./Images.css";
 import { useParams } from "react-router-dom";
 import { projects } from "../Portfolio/ProtfolioData";
@@ -8,41 +8,15 @@ import "react-lazy-load-image-component/src/effects/blur.css";
 const Images = () => {
   const { projectId } = useParams();
   const project = projects.find((proj) => proj.id === parseInt(projectId));
-
   const [sortedImages, setSortedImages] = useState([]);
-  const loadedImagesRef = useRef({});
 
   useEffect(() => {
     if (project) {
-      const verticalImages = [];
-      const horizontalImages = [];
-
-      let loadedCount = 0;
-      project.galleryImages.forEach((image) => {
-        const img = new Image();
-        img.src = image.original;
-        img.onload = () => {
-          const aspectRatio = img.width / img.height;
-          if (aspectRatio > 1) {
-            horizontalImages.push(image);
-          } else {
-            verticalImages.push(image);
-          }
-
-          loadedCount++;
-          if (loadedCount === project.galleryImages.length) {
-            setSortedImages([...verticalImages, ...horizontalImages]);
-          }
-        };
-      });
+      setSortedImages(project.galleryImages);
     }
   }, [project]);
 
   if (!project) return <p>Project not found</p>;
-
-  const handleImageLoad = (index) => {
-    loadedImagesRef.current[index] = true;
-  };
 
   return (
     <div className="gallery-container" style={{ marginTop: "6rem" }}>
@@ -50,21 +24,21 @@ const Images = () => {
         {project.title} Gallery <span className="yellow-line"></span>
       </h1>
       <h1>{project.galleryTitle}</h1>
+
       <div className="grid-gallery">
         {sortedImages.map((image, index) => {
           const isLarge = index === 0;
-          const isLoaded = loadedImagesRef.current[index];
 
           return (
             <LazyLoadImage
               key={index}
               alt={`Gallery image ${index + 1}`}
               src={image.original}
-              effect={isLoaded ? undefined : "blur"}
+              placeholderSrc={image.original}
+              effect="blur"
               className={`gallery-item ${isLarge ? "large" : ""} item-${index + 1}`}
-              wrapperClassName={`gallery-item-wrapper ${isLarge ? "large" : ""} ${isLoaded ? "loaded" : "loading"
-                }`}
-              onLoad={() => handleImageLoad(index)}
+              wrapperClassName={`gallery-item-wrapper ${isLarge ? "large" : ""}`}
+              loading="lazy"
             />
           );
         })}
