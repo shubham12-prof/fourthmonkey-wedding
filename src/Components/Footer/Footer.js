a460c1a87e9bba62f202c1cdee25b4f968b5b774
shubham12-prof <shubhsemwal12@gmail.com>
2025-07-11 14:42:16 +0530

    footer copyright change

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -47,7 +47,7 @@ const Footer = () => {
         </div>
       </div>
       <div className="footer-bottom">
-        <p>© Take Pride 2025</p>
+        <p>© 2025 Take Pride 25</p>
       </div>
     </footer>
   );
