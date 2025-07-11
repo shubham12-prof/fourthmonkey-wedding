952f20abb4656aa573bad1a5a1e9afee8cc3827f
shubham12-prof <shubhsemwal12@gmail.com>
2025-07-11 13:53:02 +0530

    router changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const LoadingScreen = () => {
   return (
@@ -29,11 +29,11 @@ const RootComponent = () => {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <React.StrictMode>
       <Suspense fallback={<LoadingScreen />}>
         <RootComponent />
       </Suspense>
     </React.StrictMode>
-  </HashRouter>
+  </BrowserRouter>
 );
